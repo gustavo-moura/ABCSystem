d9f94ed71ceabd01ee8fb393d09803a63bc9594d
Yago Pessoa <yagorp15@gmail.com>
2018-06-19 20:37:39 -0300

    Novas funcoes de query

--- queries.py
+++ queries.py
@@ -20,6 +20,7 @@ for row in curs:
 
 """
 
+# login
 def checkLogin(user, pwd):
     try:
         query = "SELECT E.login, P.senha, E.funcao, E.documento FROM Pessoa P JOIN Empregado E ON P.id_pessoa=E.id_pessoa where E.login='"+user+"' AND P.senha='"+pwd+"'"
@@ -43,11 +44,53 @@ def checkLogin(user, pwd):
         print("Login nao efetuado...")
         return False
 
+# relatorio 1 - cartoes vencidos
+def rel_cartaoVencido():
+    try:
+        query = "SELECT P.nome, P.email, V.data_venda, C.cartao_tipo, C.cartao_numero, C.cartao_validade_mes, C.cartao_validade_ano FROM Cliente C JOIN Pessoa P ON P.id_pessoa=C.id_pessoa JOIN Venda V ON C.id_cliente=V.id_cliente WHERE TO_DATE(C.CARTAO_VALIDADE_MES||'/'||C.CARTAO_VALIDADE_ANO, 'MM/YYYY')<SYSDATE"
+        curs = orcl.cursor()
+        curs.execute(query)
+        
+        rows = curs.fetchmany(numRows=15)
+
+        commit = "COMMIT"
+        curs = orcl.cursor()
+        curs.execute(commit)
+        return rows
+    except:
+        print("Erro na query...")
+        return None
+
+# relatorio 2 - cartoes vencidos filtro por nome
+def rel_cartaoVencidoNome(nome):
+    try:
+        query = "SELECT P.nome, P.email, V.data_venda, C.cartao_tipo, C.cartao_numero, C.cartao_validade_mes, C.cartao_validade_ano FROM Cliente C JOIN Pessoa P ON P.id_pessoa=C.id_pessoa JOIN Venda V ON C.id_cliente=V.id_cliente WHERE TO_DATE(C.CARTAO_VALIDADE_MES||'/'||C.CARTAO_VALIDADE_ANO, 'MM/YYYY')<SYSDATE AND P.NOME='%"+nome+"%'"
+        curs = orcl.cursor()
+        curs.execute(query)
+
+        rows = curs.fetchall()
+
+        commit = "COMMIT"
+        curs = orcl.cursor()
+        curs.execute(commit)
+
+        return rows
+    except:
+        return None
+
 # test login successful
-checkLogin("adventure-works\jianshuo0", "OHTnyBvL8Z29tVGOT1/XKjVzsJCf9XezJf5TScu1fa0")
+#checkLogin("adventure-works\jianshuo0", "OHTnyBvL8Z29tVGOT1/XKjVzsJCf9XezJf5TScu1fa0")
 
 # test login failure
-checkLogin("uasdiusa", "zzzwe")
+#checkLogin("uasdiusa", "zzzwe")
+
+# test rel 1
+#rows = rel_cartaoVencido()
+#for row in rows:
+#    print(row)
+
+# test rel 2 (FAIL)
+#print(rel_cartaoVencidoNome('Marcel'))
 
 # closing connection
 orcl.close()
